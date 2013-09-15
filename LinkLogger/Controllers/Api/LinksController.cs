2ea05f86728244109c5c98c36811ab0b2a2361df
Henri Tuomola <htuomola@gmail.com>
2013-09-15 11:46:39 +0300

    HTTP Post test fixed

--- LinkLogger/Controllers/Api/LinksController.cs
+++ LinkLogger/Controllers/Api/LinksController.cs
@@ -1,30 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using LinkLogger.DataAccess;
+using Microsoft.Ajax.Utilities;
 
 namespace LinkLogger.Controllers.Api
 {
     public class LinksController : ApiController
     {
-        public async Task<Link> Get(int id)
+        public async Task<LinkModel> Get(int id)
         {
             using (var context = new LinkLoggerContext())
             {
                 Link link = await context.Links.FindAsync(id);
-                return link;
+                return MapLinkToLinkModel(link);
             }
         }
 
-        public async Task<HttpResponseMessage> Post(LinkRequest request)
+        public async Task<IEnumerable<LinkModel>> Get()
+        {
+            using (var context = new LinkLoggerContext())
+            {
+                var links = await context.Links.OrderByDescending(l => l.RegisteredAt).Take(10).ToArrayAsync();
+                return links.Select(MapLinkToLinkModel);
+            }
+        }
+
+        public async Task<string> Post(LinkModel request)
         {
             if (!ModelState.IsValid)
             {
                 var errors = ModelState.SelectMany(s => s.Value.Errors).Select(err => err.Exception.Message);
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid model state: "+string.Join(",", errors));
+                var msg = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                          {
+                              Content =
+                                  new StringContent(
+                                  "Invalid model state: " +
+                                  string.Join(",", errors))
+                          };
+                throw new HttpResponseException(msg);
             }
 
             using (var context = new LinkLoggerContext())
@@ -39,26 +59,43 @@ namespace LinkLogger.Controllers.Api
                 context.Links.Add(link);
                 int rowsAdded = await context.SaveChangesAsync();
                 // TODO: log
-                if (rowsAdded != 1) new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                if (rowsAdded != 1) throw new HttpResponseException(HttpStatusCode.InternalServerError);
 
                 //var response = new LinkResponse()
                 //               {
                 //                   Id = link.Id
                 //               };
 
-                return Request.CreateResponse(HttpStatusCode.OK, link.Id);
+                return Url.Link("DefaultApi", new {controller = "links", id = link.Id});
             }
         }
 
-
+        private LinkModel MapLinkToLinkModel(Link arg)
+        {
+            return new LinkModel()
+                   {
+                       Channel = arg.Channel,
+                       Id = arg.Id,
+                       PostedAt = arg.RegisteredAt,
+                       User = arg.Sender,
+                       Url = arg.Url
+                   };
+        }
     }
 
-    public class LinkRequest
+    public class LinkModel
     {
+        public int? Id { get; set; }
+        
+        [Required]
         public string Url { get; set; }
-
+        
+        [Required]
         public string User { get; set; }
-
+        
+        [Required]
         public string Channel { get; set; }
+        
+        public System.DateTime? PostedAt { get; set; }
     }
 }
